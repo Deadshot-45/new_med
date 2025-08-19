758f672859aaa3402b7cca73c54bc77690124bb0
Deadshot-45 <sahumayank8179@gmail.com>
2025-08-19 22:34:57 +0530

    Nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Footer from "./common/Footer";
+import Navbar from "./common/Navbar";
 
 const About = () => <h2>About Page</h2>;
 
 const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [cart, setCart] = useState([]);
+  // Scroll helper
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <Router>
+      <Navbar
+        cartCount={cart.length}
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+        scrollToSection={scrollToSection}
+      />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              cart={cart}
+              setCart={setCart}
+              scrollToSection={scrollToSection}
+              isMenuOpen={isMenuOpen}
+              setIsMenuOpen={setIsMenuOpen}
+            />
+          }
+        />
         <Route path="/about" element={<About />} />
-
-        
       </Routes>
       <Footer />
     </Router>
